ea1cc5f34be539d391c96de87df2e66cc064db09
anatolykabakov <anatoly.kabakov@mail.ru>
2021-01-09 12:49:48 +0300

    changed format of output message in arduino file

--- first_project/arduino/master/master.ino
+++ first_project/arduino/master/master.ino
@@ -15,10 +15,10 @@ int button2Raw = 0;
 int button3Raw = 0;
 int HYDRO_UP = 0;
 int HYDRO_DOWN = 0;
-int DEBUG = 1;
+int DEBUG = 0;
 
 void setup() {
-  Serial.begin(115200); // Default communication rate of the Bluetooth module
+  Serial.begin(9600); // Default communication rate of the Bluetooth module
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
@@ -73,14 +73,30 @@ void loop() {
     Serial.print(button3State);
     Serial.println("");
   } else {
-    Serial.write(JoysticLeftXAxis); // Dividing by 4 for converting from 0 - 1023 to 0 - 256, (1 byte) range
-    Serial.write(JoysticLeftYAxis);
-    Serial.write(JoysticRightXAxis); // Dividing by 4 for converting from 0 - 1023 to 0 - 256, (1 byte) range
-    Serial.write(JoysticRightYAxis);
-    Serial.write(button1State);
-    Serial.write(button2State);
-    Serial.write(button3State);
-    Serial.write(13);
+    byte message[]= {0x41, 0x54, 0x2B, 0x4D, 0x45, 0x53, 0x48, 0x00, 0xFF, 0xFF, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x13, 0x0D, 0x0A};
+    message[10] = JoysticLeftXAxis;
+    message[11] = JoysticLeftYAxis;
+    message[12] = JoysticRightXAxis;
+    message[13] = JoysticRightYAxis;
+    message[14] = button1State;
+    message[15] = button2State;
+    message[16] = button3State;
+    Serial.write(message, sizeof(message));
+    //Serial.write('\r\n'); //  Carriage Return
+//    Serial.println('AT+MESH0FFFF11223344', HEX);
+//    for (int i=0;i<sizeof(message);i++) {
+//      Serial.print(message[i], HEX);
+//    }
+//    Serial.println("41542B4D45534800FFFF112233440D0A", HEX);
+//    Serial.print(message, HEX);
+//    Serial.write(JoysticLeftXAxis); // Dividing by 4 for converting from 0 - 1023 to 0 - 256, (1 byte) range
+//    Serial.write(JoysticLeftYAxis);
+//    Serial.write(JoysticRightXAxis); // Dividing by 4 for converting from 0 - 1023 to 0 - 256, (1 byte) range
+//    Serial.write(JoysticRightYAxis);
+//    Serial.write(button1State);
+//    Serial.write(button2State);
+//    Serial.write(button3State);
+//    Serial.write(13);
   }
-  delay(10);
+  delay(50);
 }
